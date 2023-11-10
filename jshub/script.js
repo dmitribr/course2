3f5e9b15847b2225f7e2865b78403e63e68bdea4
Dmitrybr <dmiitriib@gmail.ru>
2023-11-11 01:15:55 +0900

    lesson15

--- jshub/script.js
+++ jshub/script.js
@@ -3,8 +3,43 @@
 const obj = {
     name: "Vika",
     age: 33,
-    city: "Vladivostok"
+    city: "Vladivostok",
+    test: {
+        test2: {
+            userName: "Grisha"
+        }
+    }
 };
 
+obj.lastName = 4544;
 
-console.log(obj);
+console.log(obj["test"].test2.userName);
+
+//массив частный случай объекта
+let arr = ["tre", "dfhf", 3, "bbb"];
+console.log(arr[3]);
+
+//task:1
+const storeName = "Newshop";
+const storeDescription = {
+    budget: 10000,
+    employees: ['Jon', 'Tom', 'Luis'],
+    products: {
+        apple: 145,
+        orange: 98
+    },
+    open: true
+};
+
+//#13
+//alert("no js");
+//const result = confirm('why?');
+//const result = prompt('Ваш возраст', '18'); //возвращает всегда строковое значение.если перед prompt поставить + то вернёт number
+const result = [];
+
+result[0] = prompt('Ваше имя');
+result[1] = prompt('Ваша фамилия');
+result[2] = prompt('Ваш возраст');
+
+document.write(result);
+
