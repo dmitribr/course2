38abc51ecdcf7ae0a863a68fe8ba3c660689ebad
Dmitrybr <dmiitriib@gmail.ru>
2023-11-11 18:54:32 +0900

    lesson19

--- jshub/script.js
+++ jshub/script.js
@@ -1,45 +1,59 @@
 "use strict"
 
-const obj = {
-    name: "Vika",
-    age: 33,
-    city: "Vladivostok",
-    test: {
-        test2: {
-            userName: "Grisha"
-        }
-    }
+// const obj = {
+//     name: "Vika",
+//     age: 33,
+//     city: "Vladivostok",
+//     test: {
+//         test2: {
+//             userName: "Grisha"
+//         }
+//     }
+// };
+
+// obj.lastName = 4544;
+
+// console.log(obj["test"].test2.userName);
+
+// //массив частный случай объекта
+// let arr = ["tre", "dfhf", 3, "bbb"];
+// console.log(arr[3]);
+
+// //task:1
+// const storeName = "Newshop";
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Jon', 'Tom', 'Luis'],
+//     products: {
+//         apple: 145,
+//         orange: 98
+//     },
+//     open: true
+// };
+
+// //#13
+// //alert("no js");
+// //const result = confirm('why?');
+// //const result = prompt('Ваш возраст', '18'); //возвращает всегда строковое значение.если перед prompt поставить + то вернёт number
+// const result = [];
+
+// result[0] = prompt('Ваше имя');
+// result[1] = prompt('Ваша фамилия');
+// result[2] = prompt('Ваш возраст');
+
+// document.write(result);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    provat: false
 };
 
-obj.lastName = 4544;
+const lastMovie = prompt('Один из последних просмотренных фильмов?');
+const rating = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[lastMovie] = rating;
 
-console.log(obj["test"].test2.userName);
-
-//массив частный случай объекта
-let arr = ["tre", "dfhf", 3, "bbb"];
-console.log(arr[3]);
-
-//task:1
-const storeName = "Newshop";
-const storeDescription = {
-    budget: 10000,
-    employees: ['Jon', 'Tom', 'Luis'],
-    products: {
-        apple: 145,
-        orange: 98
-    },
-    open: true
-};
-
-//#13
-//alert("no js");
-//const result = confirm('why?');
-//const result = prompt('Ваш возраст', '18'); //возвращает всегда строковое значение.если перед prompt поставить + то вернёт number
-const result = [];
-
-result[0] = prompt('Ваше имя');
-result[1] = prompt('Ваша фамилия');
-result[2] = prompt('Ваш возраст');
-
-document.write(result);
-
+console.log(personalMovieDB);
