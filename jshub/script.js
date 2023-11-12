b4680e697fe7b10b5411f7a3ecd1d11b7b43070f
Dmitrybr <dmiitriib@gmail.ru>
2023-11-12 14:20:57 +0900

    task23.3

--- jshub/script.js
+++ jshub/script.js
@@ -136,7 +136,7 @@ gra: for (i = 0 ; i <= line ; i++) {
     result += '\n';
 }
 console.log(result);
-//задачи 23
+//задачи 23 (1 раздел)
 for (i = 5 ; i <= 10 ; i++) {
     console.log(i);
 }
@@ -190,4 +190,77 @@ for (i = 5 ; i <= 10 ; i++) {
     console.log(arrayOfNumbers);
 }
 
+// задача 2 раздел
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (i = 0 ; i < arr.length ; i++) {
+        result[i] = arr[i];
+        console.log(result);
+    }
+    
+}
+firstTask();
+
+
+//2.2
+
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (i = 0 ; i < data.length ; i++) {
+        if (typeof(data[i]) === "string") {
+            data[i] += " - done";
+            console.log(data);
+        } else {
+            data[i] = data[i] * 2;
+            console.log(data);
+        }
+    }
+     return data;
+};
+secondTask();
   
+//2.3
+
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+    let num = data.length - 1;
+    
+    for (i = 0 ; i < data.length ; i++) {
+        
+        result[i] = data[num];
+        num--;
+        console.log(result);
+    }
+    
+}
+thirdTask();
+
+//задача ёлочка. 3
+
+const lines = 5;
+let result = '';
+let dot = '*';
+let doto = dot;    
+let prom = '';
+
+for (i = 0 ; i < lines ; i++) {
+    for (j = i ; j < lines - 1 ; j++){
+        prom = `${prom} `;
+    }
+    
+    result = `${result}${prom}${dot}\n`;
+    dot = `${dot}${doto}${doto}`;
+    prom = '';
+    
+};
+console.log(result);
+
+//25
+
